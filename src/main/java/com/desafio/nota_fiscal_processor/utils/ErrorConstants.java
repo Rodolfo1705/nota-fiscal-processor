06b606eaeb3681fb9d61dfc1d9e7e7a77dbccc9b
rodolfo abrahao <rodolfo.araujo@solutis.com.br>
2025-02-04 23:16:49 -0300

    feat: adding error constants file

---
+++ src/main/java/com/desafio/nota_fiscal_processor/utils/ErrorConstants.java
@@ -0,0 +1,15 @@
+package com.desafio.nota_fiscal_processor.utils;
+
+public final class ErrorConstants {
+    private ErrorConstants() {}
+
+    public static final String INVALID_ID = "ID inválido.";
+
+    public static final String INVALID_CNPJ = "CNPJ inválido.";
+
+    public static final String INVALID_VALOR = "Valor inválido.";
+
+    public static final String INVALID_FIELDS_QUANTITY = "Quantidade de campos inválida.";
+
+    public static final String SHOULD_BE_POSITIVE_VALOR = "O valor deve ser igual ou maior que zero.";
+}
